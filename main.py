9211017c27e82de2bece6bff57561e0908afd1d6
sunil-pawar <saccha.sunil@gmail.com>
2020-12-29 14:46:39 +0000

    updated with the response test and exception block

--- main.py
+++ main.py
@@ -6,24 +6,34 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 
-URL = 'http://ec2-54-184-81-169.us-west-2.compute.amazonaws.com/now/'
-driver = webdriver.Chrome('./chromedriver')
-driver.get(URL)
-html = driver.page_source
-soup = BeautifulSoup(html, 'html.parser')
-results = soup.p.string
-localtime = dat.datetime.now()
-current_time = localtime.strftime("%H:%M:%S")
-py_time = dat.datetime.strptime(current_time,"%H:%M:%S")
-server_time = dat.datetime.strptime(results,"%H:%M:%S")
-if py_time == server_time:
-    print("both the server time and browser time macthes")
-else:
-    print("the server time and local time doesnt macth")
+URL = 'http://ec2-54-184-81-169.us-west-2.compute.amazonaws.com/now'
+#URL = 'https://google.com'
+try:
 
-print("the browser time is " + current_time)
-print("the server time is " + results)
-driver.close()
-
-
-
+    response_code = requests.get(URL).status_code
+    if response_code==200:
+            print("the" +URL+ "is  OK and working with status code" + str(response_code))
+            driver = webdriver.Chrome('./chromedriver')
+            driver.get(URL)
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
+            results = soup.p.string
+            localtime = dat.datetime.now()
+            current_time = localtime.strftime("%H:%M:%S")
+            py_time = dat.datetime.strptime(current_time,"%H:%M:%S")
+            server_time = dat.datetime.strptime(results,"%H:%M:%S")
+            if py_time == server_time:
+                print("both the server time and browser time macthes")
+                print("the browser time is " + current_time)
+                print("the server time is " + results)
+            else:
+                print("the server time and local time doesnt macth")
+                print("the browser time is " + current_time)
+                print("the server time is " + results)
+            driver.close()
+    else:
+         print("the "+URL+" is NOT OK as it is responding with statust " +str(response_code) )
+except requests.ConnectionError as c:
+    print("The URL is not responding it is NOT OKAY  ...." + str(c))
+except AttributeError as a:
+    print("the URL provided is not intended to be used for the script " + URL)
